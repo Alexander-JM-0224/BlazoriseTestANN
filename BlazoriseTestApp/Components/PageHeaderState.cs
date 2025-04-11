2f79b304b51800fdd00d59740ffc4e4d90ea6cda
Alexander <ann@jama.nl>
2025-04-11 10:56:01 +0200

    Add project files.

---
+++ BlazoriseTestApp/Components/PageHeaderState.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazoriseTestApp.Components
+{
+    public class PageHeaderState
+    {
+        public event Action? OnChange;
+
+        private string? _title;
+        public string? Title
+        {
+            get => _title;
+            set
+            {
+                _title = value;
+                NotifyStateChanged();
+            }
+        }
+
+        private RenderFragment? _actions;
+        public RenderFragment? Actions
+        {
+            get => _actions;
+            set
+            {
+                _actions = value;
+                NotifyStateChanged();
+            }
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
